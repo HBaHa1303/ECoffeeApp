8b1904726fa0e0e24814b359fbfd87eaab176052
HBaHa1303 <hoangbaha1303@gmail.com>
2026-03-25 13:46:11 +0700

    feat: change repo folder

--- ECoffee.Application/Repositories/IUserRepository.cs
+++ ECoffee.Application/Repositories/IUserRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ECoffee.Application
+namespace ECoffee.Application.Repositories
 {
     public interface IUserRepository
     {
